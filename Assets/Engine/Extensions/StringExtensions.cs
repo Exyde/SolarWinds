5832323a60ea5cfead437e943ad6cb691956c44f
Younes <younesskate@hotmail.fr>
2024-04-06 12:13:29 +0200

    Random Char, Random String

--- Assets/Engine/Extensions/StringExtensions.cs
+++ Assets/Engine/Extensions/StringExtensions.cs
@@ -9,4 +9,22 @@ public static class StringExtension {
     public static string RemoveIllegalCharacters(this string str, string sep = ""){
         return string.Join(sep, str.Split(System.IO.Path.GetInvalidFileNameChars()));
     }
+
+    public static string GetRandomString(int length)
+    {
+        var random = "";
+
+        for (var i = 0; i < length; i++)
+        {
+            random += GetRandomLetter();
+        }
+
+        return random;
+    }
+
+    static System.Random _random = new ();
+    public static string GetRandomLetter()
+    {
+        return ((char)_random.Next('a', 'z')).ToString();
+    }
 }
---
+++ Assets/_Sources/Scripts/Genetics/Shakespear.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using EnhancedEditor;
+using UnityEngine;
+
+namespace _Sources.Scripts.Genetics
+{
+    public class Shakespear : MonoBehaviour
+    {
+        public int wordLength;
+
+        [Button(ActivationMode.Always)]
+        public void LogRandomString()
+        {
+            Debug.Log(StringExtension.GetRandomString(wordLength));
+        }
+    }
+}
