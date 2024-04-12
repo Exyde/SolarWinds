ae13af543bac72ef88121b162128f0e496d48896
Younes <younesskate@hotmail.fr>
2024-04-12 18:00:11 +0200

    Riding the Phoenix wip

--- Assets/Plugins/Pigeon/Bird Flocks/Scripts/LandingSpotController.cs
+++ Assets/Plugins/Pigeon/Bird Flocks/Scripts/LandingSpotController.cs
@@ -1,13 +1,14 @@
 ///	Copyright Unluck Software /// www.chemicalbliss.com																															
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class LandingSpotController : MonoBehaviour
 {
 	public bool _rotateAfterLanding = true;							// Rotates the bird to the rotation of the landing spot
 	public bool _randomRotate = true;                               // Random rotation when a bird lands
-	public Vector2 _autoCatchDelay = new Vector2(10.0f, 20.0f);     // Random Min/Max time for landing spot to make a bird land
-	public Vector2 _autoDismountDelay = new Vector2(10.0f, 20.0f);  // Random Min/Max time for birds to automaticly fly away from landing spot
+	public Vector2 _autoCatchDelay = new (10.0f, 20.0f);		// Random Min/Max time for landing spot to make a bird land
+	public Vector2 _autoDismountDelay = new (10.0f, 20.0f);		// Random Min/Max time for birds to automaticly fly away from landing spot
 	public float _maxBirdDistance = 20.0f;                          // The maximum distance to a bird for it to land
 	public float _minBirdDistance = 5.0f;                           // The minimum distance to a bird for it to land
 	public bool _takeClosest;                                       // Toggle this to make landingspots make the closest bird to it land
@@ -32,55 +33,54 @@ public class LandingSpotController : MonoBehaviour
 	public bool _abortLanding;
 	[Range(1, 20)]
 	public float _abortLandingTimer = 10f;        // If birds have a tendency to get stuck while landing this can be used as a safety measure 
-	
+
+	private List<LandingSpot> spots;
 	public void Start() {
 		if (_transformCache == null) _transformCache = transform;
 		if (_flock == null) {
 			_flock = (FlockController)FindObjectOfType(typeof(FlockController));
 			Debug.Log(this + " has no assigned FlockController, a random FlockController has been assigned");
 		}
+
+		spots = new List<LandingSpot>();
+
+		for (int i = 0; i < _transformCache.childCount; i++)
+		{
+			spots.Add(transform.GetChild(i).GetComponent<LandingSpot>());
+		}
+		
 		if (_landOnStart) {
 			StartCoroutine(InstantLandOnStart(.1f));
 		}
 	}
 
 	public void ScareAll() {
-		for (int i = 0; i < _transformCache.childCount; i++) {
-			if (_transformCache.GetChild(i).GetComponent<LandingSpot>() != null) {
-				LandingSpot spot = _transformCache.GetChild(i).GetComponent<LandingSpot>();
-
-				spot.ReleaseFlockChild();
-			}
+		foreach (var landingSpot in spots)
+		{
+			landingSpot.ReleaseFlockChild();
 		}
 	}
 
 	public void LandAll() {
-		for (int i = 0; i < _transformCache.childCount; i++) {
-			if (_transformCache.GetChild(i).GetComponent<LandingSpot>() != null) {
-				LandingSpot spot = _transformCache.GetChild(i).GetComponent<LandingSpot>();
-				StartCoroutine(spot.GetFlockChild(0.0f, 2.0f));
-			}
+		foreach (var landingSpot in spots)
+		{
+			StartCoroutine(landingSpot.GetFlockChild(0.0f, 2.0F));
 		}
 	}
 
-	//This function was added to fix a error with having a button calling InstantLand
 	private IEnumerator InstantLandOnStart(float delay) {
 		yield return new WaitForSeconds(delay);
-		for (var i = 0; i < _transformCache.childCount; i++)
+		foreach (var landingSpot in spots)
 		{
-			if (_transformCache.GetChild(i).GetComponent<LandingSpot>() == null) continue;
-			LandingSpot spot = _transformCache.GetChild(i).GetComponent<LandingSpot>();
-			spot.InstantLand();
+			landingSpot.InstantLand();
 		}
 	}
 
 	public IEnumerator InstantLand(float delay) {
 		yield return new WaitForSeconds(delay);
-		for (var i = 0; i < _transformCache.childCount; i++)
+		foreach (var landingSpot in spots)
 		{
-			if (_transformCache.GetChild(i).GetComponent<LandingSpot>() == null) continue;
-			LandingSpot spot = _transformCache.GetChild(i).GetComponent<LandingSpot>();
-			spot.InstantLand();
+			landingSpot.InstantLand();
 		}
 	}
 }
---
+++ Assets/_Sources/Scripts/Player/EntityRidingController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Systems.Entities;
+using UnityEngine;
+
+namespace _Sources.Scripts.Player
+{
+    
+    //Utiliser une interface IRidable ?
+    public class EntityRidingController : MonoBehaviour
+    {
+
+        private bool isRiding;
+        [SerializeField] private float detectionRadius = 1f;
+        [SerializeField] private LayerMask entityLayer;
+
+        [SerializeField] private Transform attachedEntity;
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.X))
+            {
+                ToggleRideState();
+            }
+        }
+
+        private void LateUpdate()
+        {
+            if (attachedEntity != null)
+            {
+                transform.position = attachedEntity.position;
+            }
+        }
+
+        private void ToggleRideState()
+        {
+            if (isRiding)
+            {
+                SetFree();
+                return;
+            }
+
+            StartRideEntity();
+        }
+
+        private void SetFree()
+        {
+            isRiding = false;
+            attachedEntity = null;
+        }
+
+        private void StartRideEntity()
+        {
+            var colliders = Physics.OverlapSphere(transform.position, detectionRadius, entityLayer);
+            {
+                foreach (var coll in colliders )
+                {
+                    if (!coll.TryGetComponent(out Entity entity)) continue;
+                    AttachTo(entity);
+                    break;
+                }
+            }
+        }
+
+        private void AttachTo(Entity entity)
+        {
+            //Todo : Implement holder interface
+            isRiding = true;
+            attachedEntity = entity.transform;
+        }
+    }
+}
